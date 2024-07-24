a9489dfb53fd131da8da5d14162e5511eaf92344
afrian2416 <afrian.r12@gmail.com>
2024-07-24 14:12:12 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import math
+import streamlit as st
+
+st.title("Volume Bola KalKulator")
+
+radius = st.number_input("Masukkan jari-jari bola (r):", min_value=0.0, format="%.2f")
+
+def volume_bola(radius):
+    return (4/3) * math.pi * radius**3
+
+if st.button("Hitung Volume"):
+    volume = volume_bola(radius)
+    st.success(f"Volume bola dengan jari-jari {radius:.2f} adalah {volume:.2f} unit kubik.")
